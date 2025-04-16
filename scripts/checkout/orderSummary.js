5bc385aa7eed1906711581300dd3f567fc8a1f13
AnujRawat <rawatanuj0063@gmail.com>
2025-04-16 12:58:45 +0530

    Insted of DOM, regenrated HTML for delete link

--- scripts/checkout/orderSummary.js
+++ scripts/checkout/orderSummary.js
@@ -124,10 +124,11 @@ export function renderOrderSummary(){
 				const productId = link.dataset.productId;
 				removeFromCart(productId);
 				
-				const container = document.querySelector(
-					`.js-cart-item-container-${productId}`
-				);
-				container.remove();
+				// const container = document.querySelector(
+				// 	`.js-cart-item-container-${productId}`
+				// );
+				// container.remove();
+				renderOrderSummary();
 				updateCartQuantity();
 				renderPaymentSummary();
 			});
