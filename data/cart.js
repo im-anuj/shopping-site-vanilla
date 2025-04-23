f76b210768c049795b7c65a3c24168f2b281aa5a
AnujRawat <rawatanuj0063@gmail.com>
2025-04-23 12:30:31 +0530

    Create some more tests

--- data/cart.js
+++ data/cart.js
@@ -1,3 +1,5 @@
+import { validDeliveryOption } from "./deliveryOptions.js";
+
 export let cart;
 
 loadFromStorage();
@@ -86,6 +88,14 @@ export function updateDeliveyOption(productId, deliveryOptionId){
             matchingItem = cartItem;
         }
     });
+
+    if(!matchingItem){
+        return;
+    }
+    
+    if(!validDeliveryOption(deliveryOptionId)){
+        return;
+    }
 
     matchingItem.deliveryOptionId = deliveryOptionId;
 
