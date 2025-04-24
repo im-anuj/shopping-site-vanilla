9ea12392cf15608d987aa4c5770442b7bcc7298a
AnujRawat <rawatanuj0063@gmail.com>
2025-04-24 12:23:50 +0530

    Create class version of cart

--- data/cart-oop.js
+++ data/cart-oop.js
@@ -6,7 +6,7 @@ function Cart(localStorageKey){
         cartItems: undefined,
     
         loadFromStorage(){  // shorthand property loadFromStorage: function() {}
-            this.cartItems = JSON.parse(localStorage.getItem(`${localStorageKey}`));
+            this.cartItems = JSON.parse(localStorage.getItem(localStorageKey));
         
             if(!this.cartItems){
                 this.cartItems = [{
@@ -22,7 +22,7 @@ function Cart(localStorageKey){
         },
     
         saveToStorage(){
-            localStorage.setItem(`${localStorageKey}`, JSON.stringify(this.cartItems));
+            localStorage.setItem(localStorageKey, JSON.stringify(this.cartItems));
         },
     
         addToCart(productId, quantity) {
