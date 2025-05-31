b122aae25ed8c0c44b2e2e0695786fbb86d5e050
AnujRawat <rawatanuj0063@gmail.com>
2025-05-31 15:36:54 +0530

    Make tacking page interactive

--- scripts/tracking.js
+++ scripts/tracking.js
@@ -2,6 +2,7 @@ import { getOrder } from "../data/orders.js";
 import { getProduct, loadProductsFetch } from "../data/products.js";
 import { calculateDeliveryDateFrom } from "../data/deliveryOptions.js";
 import { findDeliveryOption } from "./orders.js";
+import dayjs from "https://unpkg.com/supersimpledev@8.5.0/dayjs/esm/index.js";
 
 async function loadPage(){
     await loadProductsFetch();
@@ -27,6 +28,11 @@ async function loadPage(){
         deliveryDate = rawDate.format('dddd, MMMM D');
     }
 
+    const today = dayjs();
+    const orderTime = dayjs(order.orderTime);
+    const deliveyTime = dayjs(productDetails.estimatedDeliveryTime);
+    const percentProgress = ((today - orderTime) / (deliveyTime - orderTime)) * 100;
+
     const trackingHTML = `
         <a class="back-to-orders-link link-primary" href="orders.html">
           View all orders
@@ -47,19 +53,25 @@ async function loadPage(){
         <img class="product-image" src="${product.image}">
 
         <div class="progress-labels-container">
-          <div class="progress-label">
+          <div class="progress-label ${
+            percentProgress < 50 ? 'current stauts' : ''
+          }">
             Preparing
           </div>
-          <div class="progress-label current-status">
+          <div class="progress-label ${
+            percentProgress >= 50 && percentProgress < 100 ? 'current stauts' : ''
+          }">
             Shipped
           </div>
-          <div class="progress-label">
+          <div class="progress-label ${
+            percentProgress >= 100 ? 'current stauts' : ''
+          }">
             Delivered
           </div>
         </div>
 
         <div class="progress-bar-container">
-          <div class="progress-bar"></div>
+          <div class="progress-bar" style="width: ${percentProgress}%;"></div>
         </div>
     `;
     
